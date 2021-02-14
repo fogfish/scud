c06b58b304d77c1891852c4f2977338887c05808
fogfish <dmkolesnikov@gmail.com>
2021-02-14 18:01:54 +0200

    custom hash to golang lambda

--- lib/runtime.ts
+++ lib/runtime.ts
@@ -8,13 +8,42 @@
 import * as cdk from '@aws-cdk/core'
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as sys from 'child_process'
+import * as path from 'path'
+import * as crypto from 'crypto'
+import * as fs from 'fs'
 
-//
-// Bundles Golang Lambda function from source
-export function AssetCodeGo(path: string): lambda.Code {
-  return new lambda.AssetCode('', { bundling: gocc(path) })
+/**
+ * Bundles Golang Lambda function from source
+ * 
+ * @param sourceCodePackage - absolute path to source code of serverless app (e.g. $GOPATH/src/github.com/fogfish/scud)
+ * @param sourceCodeLambda  - relative path to lambda function, the path to main package 
+ */
+export function AssetCodeGo(sourceCodePackage: string, sourceCodeLambda: string): lambda.Code {
+  return new lambda.AssetCode('', {
+    bundling: gocc([sourceCodePackage, sourceCodeLambda].join(path.sep)),
+    assetHashType: cdk.AssetHashType.CUSTOM,
+    assetHash: hash(sourceCodePackage),
+  })
 }
 
+const hash = (source: string): string => {
+  const goFiles = new RegExp('(.*\.go)|(.*\.(mod|sum))$')
+  const sha = crypto.createHash('sha256')
+  const dirents = fs.readdirSync(source, { withFileTypes: true })
+  dirents
+    .filter(dirent => dirent.isFile())
+    .filter(dirent => goFiles.test(dirent.name))
+    .map(dirent => dirent.name)
+    .forEach(file => {
+      const data = fs.readFileSync(path.join(source, file))
+      sha.update(`<file name=${file}>`)
+      sha.update(data)
+      sha.update('</file>')
+    })
+  const codeHash = sha.digest('hex')
+  console.log(`==> ${source} ${codeHash}`)
+  return codeHash
+}
 
 const tryBundle = (outputDir: string, options: cdk.BundlingOptions): boolean => {
   if (!options || !options.workingDirectory) {
@@ -28,13 +57,12 @@ const tryBundle = (outputDir: string, options: cdk.BundlingOptions): boolean => 
   return true
 }
 
-
-const gocc = (path: string): cdk.BundlingOptions => {
+const gocc = (codepath: string): cdk.BundlingOptions => {
   const gopath = process.env.GOPATH || '/go'
 
-  const workingDirectory = path.startsWith(gopath)
-    ? `/go${path.split(gopath).join('')}`
-    : path
+  const workingDirectory = codepath.startsWith(gopath)
+    ? `/go${codepath.split(gopath).join('')}`
+    : codepath
 
   return {
     local: { tryBundle },
