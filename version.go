fe687d14639660208100f3143a7c3610b4e16970
fogfish <dmkolesnikov@gmail.com>
2024-09-14 20:15:39 +0300

    (fea) explicit version config (#26)

---
+++ version.go
@@ -0,0 +1,11 @@
+//
+// Copyright (C) 2020 - 2024 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/fogfish/scud
+//
+
+package scud
+
+const Version = "v0.10.0"
