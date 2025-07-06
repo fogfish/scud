9e5e184e0bc34d03923fdb156682c88d9040db6e
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2025-07-06 19:14:26 +0300

    update copyright at hasher.go

--- hasher.go
+++ hasher.go
@@ -1,3 +1,11 @@
+//
+// Copyright (C) 2020 - 2025 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/fogfish/scud
+//
+
 package scud
 
 import (
